115e7475db44f9b4441077d64a5ec65b7f74246a
Diego Teodoro <diegoteodoro_alves@yahoo.com.br>
2021-09-08 15:05:17 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerRepository.java
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerRepository.java
@@ -1,11 +1,9 @@
 package com.devsuperior.dsvendas.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.devsuperior.dsvendas.entities.Seller;
 
-@Repository
-public interface SellerRepository extends JpaRepository<Seller, Long>{
+public interface SellerRepository extends JpaRepository<Seller, Long> {
 
 }
--- backend/src/main/java/com/devsuperior/dsvendas/service/SellerService.java
+++ backend/src/main/java/com/devsuperior/dsvendas/service/SellerService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsvendas.dto.SellerDTO;
 import com.devsuperior.dsvendas.entities.Seller;
@@ -18,10 +19,11 @@ public class SellerService {
 	@Autowired
 	private SellerRepository sellerRepository;
 
+	@Transactional(readOnly = true)
 	public Page<SellerDTO> findAll(Pageable pageable) {
 		sellerRepository.findAll();
 		Page<Seller> result = repository.findAll(pageable);
 		return result.map(x -> new SellerDTO(x));
 	}
-
+	
 }
